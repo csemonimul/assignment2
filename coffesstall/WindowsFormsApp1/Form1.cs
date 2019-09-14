6b652e2665767914ed4ba46ba0396f4bf5bfb05e
unknown <monimul2014@gmail.com>
2019-09-14 15:02:08 +0600

    Modified

--- coffesstall/WindowsFormsApp1/Form1.cs
+++ coffesstall/WindowsFormsApp1/Form1.cs
@@ -19,30 +19,40 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string customerName, contact, address, order, item;
-            int quantity, price, totalBill;
+            //string customerName, contact, address, order, item;
+           // int quantity=0, price, totalBill;
 
-            customerName = nameTextBox.Text;
-            contact = contactTextBox.Text;
-            address = addressTextBox.Text;
-            order = orderComboBox.Text;
-            quantity = Convert.ToInt32(quantityTextBox.Text);
-            item = orderComboBox.Text;
+           
 
+        }
 
-            if (orderComboBox.Text == "Black")
+        private void butttonTextBox_Click(object sender, EventArgs e)
+        {
+            string customerName = nameTextBox.Text;
+            string  contact = contactTextBox.Text;
+            string address = addressTextBox.Text;
+            string item = orderComboBox.Text;
+            int quantity = Convert.ToInt32(quantityTextBox.Text);
+           
+
+            int price = 0;
+            int totalBill = 0;
+
+            if (item == "Black")
                 price = 120;
-            else if (orderComboBox.Text == "Cold")
+            else if (item == "Cold")
                 price = 100;
-            else if (orderComboBox.Text == "Hot")
+            else if (item == "Hot")
+                price = 90;
+            else
                 price = 80;
-            else
-                price = 70;
-            totalBill = price * quantity;
-            if (orderComboBox.Text == "--select one--") ;
-            else
-                richTextBox.Text = "\n Customer Name:" + "\n" + "Contact Number:" + "\n" + "TotalBill";
 
+            totalBill = price * quantity;
+
+            if (orderComboBox.Text == "--select one--")
+                MessageBox.Show("Select an item");
+            else
+                richTextBox.Text = "\n Customer Name:" +customerName+  "\nContact Number:" +contact+ "\n" + "Address: "+address+"\norder:"+item+"\nQuantity:"+quantity+"\nTotal Price:"+totalBill; 
         }
     }
 }
